b17be7239f0bc4bf15a6fe3cd9211a2dbea98083
soojin <ksgf988@naver.com>
2022-10-22 15:31:13 +0900

    [BE] Add: 스프링 프로젝트 추가

---
+++ backend/src/main/java/com/ssafy/gaese/GaeseApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gaese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaeseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/gaese/GaeseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.gaese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
