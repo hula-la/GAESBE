d51aaa886027036a820a6db2a18d21de00c42565
hoju <ghwnsdl999@naver.com>
2022-10-26 10:19:02 +0900

    [FE] Add: rootReducer, store 설정

--- front/src/app/store.tsx
+++ front/src/app/store.tsx
@@ -1,1 +1,37 @@
-export {}
+import {
+  configureStore,
+  combineReducers,
+  ThunkAction,
+  Action,
+} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './rootSaga';
+// 관리하는 슬라이스들
+import authReducer from '../features/auth/authSlice';
+
+// rootReducers by using combineReducers
+const rootReducers = combineReducers({
+  auth: authReducer,
+});
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+const store = configureStore({
+  // 여러 슬라이서의 리듀서를 합침
+  reducer: rootReducers,
+  middleware: middlewares,
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export default store;
