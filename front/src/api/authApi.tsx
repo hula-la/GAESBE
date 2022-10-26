7c08d5ed1bd7eed0ec4676dfa4e98c16a309762b
윤호준 <ghwnsdl999@naver.com>
2022-10-26 10:22:47 +0900

    Revert "Merge branch 'feat-FE/login' into 'master'"
    This reverts merge request !6

--- front/src/api/authApi.tsx
+++
@@ -1,6 +0,0 @@
-import client from "./client";
-
-export const loginKakao = async (data: string) => {
-  const res = await client.post('api/user/kakao', data)
-  return res
-}
--- front/src/app/rootSaga.tsx
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-// saga 관리
-import { authSagas } from '../features/auth/authSaga';
-
-// rootSaga
-export default function* rootSaga() {
-  yield all([...authSagas]);
-}
