3f88decf18344b3d9f9363710f642c455328563e
이연의 <dusdml1502@naver.com>
2022-10-27 00:39:38 +0900

    [BE] Add: redis 설정 추가
    - redis 설정 추가
    -  chat 도메인 추가

--- backend/src/main/java/com/ssafy/gaese/domain/chat/dto/Message.java
+++ backend/src/main/java/com/ssafy/gaese/domain/chat/dto/Message.java
@@ -1,4 +1,4 @@
-package com.ssafy.gaese.domain.chat;
+package com.ssafy.gaese.domain.chat.dto;
 
 
 import lombok.AllArgsConstructor;
@@ -11,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Message {
-    private String name;
+    private String nickName;
+    private String content;
 
 }
