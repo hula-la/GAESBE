31c2fc7c1f3056a9add2021606c30e39b684877b
soojin <ksgf988@naver.com>
2022-10-31 12:18:21 +0900

    [BE] Fix: cors 설정

--- backend/src/main/java/com/ssafy/gaese/global/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/gaese/global/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://k7e104.p.ssafy.io")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
