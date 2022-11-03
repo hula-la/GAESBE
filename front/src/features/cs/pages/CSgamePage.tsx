3c2cd24a7eee485a53c91e7f6b8529c65aa66d28
hoju <ghwnsdl999@naver.com>
2022-11-03 09:27:25 +0900

    [FE] Add : CS 게임 인트로 페이지 UI

--- front/src/features/cs/pages/CSgamePage.tsx
+++ front/src/features/cs/pages/CSgamePage.tsx
@@ -3,20 +3,53 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   width: 82%;
-  background-color: #232323;
   color: #ffffff;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   .title {
     margin-top: 2rem;
     height: 15%;
+    width: 25%;
+  }
+  .back::before {
+    position: fixed;
+    top: 0;
+    left: 12%;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    background-image: url(/img/background2.png);
+    filter: blur(5px);
+    z-index: -1;
+    content: '';
+  }
+  .buttons {
+    display: flex;
+  }
+  .random {
+    background-color: #f0568c;
+    width: 20vh;
+    height: 20vh;
+  }
+  .friend {
+    background-color: #ffd219;
+    width: 20vh;
+    height: 20vh;
   }
 `;
 
 const CSgamePage = () => {
   return (
     <Wrapper>
-      <img src="/img/gametitle/gametitle2.png" alt="title" className="title" />
+      <div className="back"></div>
+
+      <img src="/img/gametitle/gametitle3.png" alt="title" className="title" />
+      <div>CS를 신속 정확하게 풀어 1등을 쟁취하라!</div>
+      <div className="buttons">
+        <div className="random">랜덤 매칭</div>
+        <div className="friend">친구 매칭 </div>
+      </div>
     </Wrapper>
   );
 };
