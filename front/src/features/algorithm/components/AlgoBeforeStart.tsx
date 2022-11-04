35e78c61488d334d7be1e7686cf70ade1835aa10
SSAFY <dkshktpq@naver.com>
2022-11-04 18:03:40 +0900

    [FE] Test: test

--- front/src/features/algorithm/components/AlgoBeforeStart.tsx
+++ front/src/features/algorithm/components/AlgoBeforeStart.tsx
@@ -1,10 +1,13 @@
 
 
 function AlgoBeforeStart({ handleLeaveRoom }: any) {
-
+  const handleStart = () => {
+    console.log('게임 시작하기')
+  }
   return <>
     <h1>시작 전 대기 방</h1>
     <button onClick={handleLeaveRoom}>대기방에서 나가기</button>
+    <button onClick={handleStart}>배틀 시작하기</button>
   </>
 }
 export default AlgoBeforeStart
