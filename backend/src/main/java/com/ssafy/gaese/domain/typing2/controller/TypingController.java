371e111abc00712b4d6801544d46b25c3249d5c0
soojin <ksgf988@naver.com>
2022-11-09 11:32:55 +0900

    [BE] Fix: 타이핑 레포지토리 Enum->String 변경

--- backend/src/main/java/com/ssafy/gaese/domain/typing2/controller/TypingController.java
+++ backend/src/main/java/com/ssafy/gaese/domain/typing2/controller/TypingController.java
@@ -2,6 +2,9 @@ package com.ssafy.gaese.domain.typing2.controller;
 
 import com.ssafy.gaese.domain.typing2.application.TypingService;
 import com.ssafy.gaese.domain.typing2.dto.TypingRecordDto;
+import com.ssafy.gaese.domain.typing2.entity.TypingParagraph;
+import com.ssafy.gaese.domain.typing2.entity.TypingRecord;
+import com.ssafy.gaese.domain.typing2.repository.TypingParagraphRepository;
 import com.ssafy.gaese.security.model.CustomUserDetails;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Api(value="typing", tags={"typing"})
 @RestController
 @Slf4j
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TypingController {
 
     private final TypingService typingService;
+    private final com.ssafy.gaese.domain.typing2.repository.TypingParagraphRepository typingParagraphRepository;
 
 
     @GetMapping("/record")
@@ -30,6 +36,12 @@ public class TypingController {
     public ResponseEntity<Page<TypingRecordDto>> recordList(Pageable pageable,
                                                             @AuthenticationPrincipal CustomUserDetails userDetails){
         return ResponseEntity.ok().body(typingService.findTypingRecord(userDetails.getId(),pageable));
+    }
+
+    @GetMapping("/find")
+    @ApiOperation(value = "cs 게임 기록 조회", notes = "사용자별 알고리즘 게임 기록 조회")
+    public ResponseEntity<List<TypingParagraph>> test(int problemNum, TypingRecord.LangType langType){
+        return ResponseEntity.ok().body(typingParagraphRepository.findRandomProblem(problemNum,langType.name()));
     }
 
 }
