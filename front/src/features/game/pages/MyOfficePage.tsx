7ce53302b7e04a9be7ff130bf103d2331eefb9b6
hoju <ghwnsdl999@naver.com>
2022-11-10 09:39:22 +0900

    [FE] Fix : 충돌 해결

--- front/src/features/game/pages/MyOfficePage.tsx
+++ front/src/features/game/pages/MyOfficePage.tsx
@@ -8,28 +8,21 @@ import Level4 from '../components/Level4';
 import Level5 from '../components/Level5';
 import Level6 from '../components/Level6';
 import FriendMainPage from '../../friend/pages/FriendMainPage';
-import { attendanceRequest } from '../../../api/mypageApi'
+import { attendanceRequest } from '../../../api/mypageApi';
 
-const Friend = styled.div`
-  width: 21.75%;
-  background-color: #232323;
-  border: 2px solid red;
-  color: white;
-`;
 const MyOfficePage = () => {
-
   const attendance = async () => {
     try {
-      const res = await attendanceRequest()
-      if (res.status===200) {
-        alert('출석체크 되었습니다')
+      const res = await attendanceRequest();
+      if (res.status === 200) {
+        alert('출석체크 되었습니다');
       }
-    } catch (error:any) {
-      if (error.response.status===446) {
-        alert('오늘은 이미 출석했습니다')
+    } catch (error: any) {
+      if (error.response.status === 446) {
+        alert('오늘은 이미 출석했습니다');
       }
     }
-  }
+  };
   return (
     <>
       <Level0 attendance={attendance} />
