d9fb123eca326dbdf20ebd2f50ca4da6778743b3
이연의 <dusdml1502@naver.com>
2022-11-11 09:00:00 +0900

    [BE]알고리즘 문제 suffle 주석

--- backend/src/main/java/com/ssafy/gaese/domain/algorithm/application/AlgoProblemService.java
+++ backend/src/main/java/com/ssafy/gaese/domain/algorithm/application/AlgoProblemService.java
@@ -153,7 +153,7 @@ public class AlgoProblemService {
         System.out.println("=====> 최종 문제 수 : " + algoProblemDtoList.size());
 
         // list 섞고 그 중 10개 가져오기
-        Collections.shuffle(algoProblemDtoList);
+//        Collections.shuffle(algoProblemDtoList);
        return algoProblemDtoList.subList(0, 10);
     }
 
