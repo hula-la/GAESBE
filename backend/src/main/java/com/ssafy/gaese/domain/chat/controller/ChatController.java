282c2c65d434263956ebe658cb0a279d36275661
soojin <ksgf988@naver.com>
2022-11-13 19:57:39 +0900

    [BE] Fix: Dto 매핑 디버깅

--- backend/src/main/java/com/ssafy/gaese/domain/chat/controller/ChatController.java
+++ backend/src/main/java/com/ssafy/gaese/domain/chat/controller/ChatController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -31,7 +32,7 @@ public class ChatController {
 
     // 해당 메시지를 봤다는 신호 보내기
     @PostMapping("/chat")
-    public void checkMsg(ChatPostDto chatPostDto){
+    public void checkMsg(@RequestBody ChatPostDto chatPostDto){
         System.out.println("checkMsg");
         System.out.println(chatPostDto.toString());
         //메시지 저장
