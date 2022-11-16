6d456cdb71705ce365c61951f50275001932ca8d
hoju <ghwnsdl999@naver.com>
2022-11-16 17:29:45 +0900

    [FE] Add : 싸피 게임 중 버튼 없애기

--- front/src/features/coinflip/coinFlipSlice.tsx
+++ front/src/features/coinflip/coinFlipSlice.tsx
@@ -9,24 +9,19 @@ interface Result {
 
 interface Coin {
   result: Result | null;
-  isLoading: boolean;
 }
 
 const initialState: Coin = {
   result: null,
-  isLoading: false,
 };
 
 const coinSlice = createSlice({
   name: 'coin',
   initialState,
   reducers: {
-    requestCoinFlipStart(state, action) {
-      state.isLoading = true;
-    },
+    requestCoinFlipStart(state, action) {},
     requestCoinFlipSuccess(state, action) {
       state.result = action.payload;
-      state.isLoading = false;
     },
     requestCoinFlipError(state, action) {},
     // 결과 리셋
