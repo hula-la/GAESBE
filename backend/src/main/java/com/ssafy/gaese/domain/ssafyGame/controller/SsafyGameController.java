351706a85476ffb06f5c5caa6fd2cf5a2b423be8
GownMinYong <alsdyd320@naver.com>
2022-11-17 11:52:47 +0900

    [BE] Fix: ssafy게임 get으로 매핑 수정

--- backend/src/main/java/com/ssafy/gaese/domain/ssafyGame/controller/SsafyGameController.java
+++ backend/src/main/java/com/ssafy/gaese/domain/ssafyGame/controller/SsafyGameController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -31,7 +28,7 @@ public class SsafyGameController {
         return ResponseEntity.ok().body(ssafyGameService.flipStart(param, userDetails.getId()));
     }
 
-    @PostMapping("/five")
+    @GetMapping("/five")
     @ApiOperation(value = "알고리즘 게임 기록 등록", notes = "알고리즘 게임 기록 등록")
     public ResponseEntity<FiveResultDto> getFive(@AuthenticationPrincipal CustomUserDetails userDetails){
 
