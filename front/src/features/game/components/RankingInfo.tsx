12208ef6881ce93272e902470863d8efbc8fa367
SSAFY <qownstlr0394@naver.com>
2022-11-18 00:20:03 +0900

    [FE] Add: 랭킹페이지 css

--- front/src/features/game/components/RankingInfo.tsx
+++ front/src/features/game/components/RankingInfo.tsx
@@ -1,29 +1,132 @@
+import styled from 'styled-components';
 import { RankerInfoInterface } from '../../../models/user';
+
+const GameType = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+`;
+const GameHeader = styled.div`
+  width: 100%;
+  height: 15%;
+  display: flex;
+  justify-content: center;
+  .img {
+    width: 20%;
+    height: 30%;
+    position: relative;
+    left: 70%;
+    top: 0%;
+  }
+`;
+const GameTable = styled.div`
+  display: flex;
+  width: 100%;
+  height: 75%;
+  border: 5px solid black;
+  border-radius: 15px;
+  background-color: #ffc02d;
+  .table {
+    width: 100%;
+    height: 100%;
+    /* border: 2px solid red; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .thead {
+      width: 100%;
+      height: 20%;
+      display: flex;
+      align-items: center;
+      .tr {
+        padding-top: 1rem;
+        width: 100%;
+        display: flex;
+        justify-content: space-evenly;
+        color: black;
+      }
+    }
+    .tbody {
+      width: 100%;
+      height: 80%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      .tr {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 90%;
+        height: 15%;
+        border: 3px solid black;
+        border-radius: 10px;
+        color: black;
+        background-color: #f0568c;
+        .td1 {
+          width: 10%;
+          display: flex;
+          justify-content: center;
+        }
+        .td2 {
+          width: 50%;
+          display: flex;
+          justify-content: center;
+        }
+        .td3 {
+          display: flex;
+          width: 30%;
+          justify-content: center;
+        }
+        .td4 {
+          display: flex;
+          justify-content: center;
+          width: 10%;
+        }
+      }
+    }
+  }
+`;
 function RankingInfo({ type, rankersInfo }: any) {
+  console.log(type, 'aaaaaaaaaaaaaaaaaaaaaaaddddddd');
   return (
-    <>
-      <h1>{type}</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>닉네임</th>
-            <th>레벨</th>
-            <th>경험치</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rankersInfo.map((rankerInfo: RankerInfoInterface, index: number) => {
-            return (
-              <tr key={index}>
-                <td>{rankerInfo.nickName}</td>
-                <td>{rankerInfo.lv}</td>
-                <td>{rankerInfo.exp}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+    <GameType>
+      <GameHeader>
+        <img className="img" src="/img/crown.png" alt="asdfasdfasdf" />
+        <h2>{type} 랭킹</h2>
+      </GameHeader>
+      <GameTable>
+        <table className="table">
+          <thead className="thead">
+            <tr className="tr">
+              <th className="td1">랭킹</th>
+              <th className="td2">닉네임</th>
+              <th className="td3">레벨</th>
+              <th className="td4">경험치</th>
+            </tr>
+          </thead>
+          <tbody className="tbody">
+            {rankersInfo.map(
+              (rankerInfo: RankerInfoInterface, index: number) => {
+                return (
+                  index <= 4 && (
+                    <tr className="tr" key={index}>
+                      <td className="td1">{index + 1}</td>
+                      <td className="td2">{rankerInfo.nickName}</td>
+                      <td className="td3">Lv.{rankerInfo.lv}</td>
+                      <td className="td4">{rankerInfo.exp}</td>
+                    </tr>
+                  )
+                );
+              },
+            )}
+          </tbody>
+        </table>
+      </GameTable>
+    </GameType>
   );
 }
 export default RankingInfo;
--- front/src/features/game/pages/GameRankingPage.tsx
+++ front/src/features/game/pages/GameRankingPage.tsx
@@ -6,12 +6,97 @@ import Swal from 'sweetalert2';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
   color: white;
-  .myRankDiv {
-    display: flex;
+`;
+const MainHeader = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid red; */
+`;
+const MyRankingBlock = styled.div`
+  width: 100%;
+  height: 20%;
+  /* border: 2px solid blue; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const MyRankingHeader = styled.div`
+  display: flex;
+  width: 90%;
+  height: 30%;
+  /* border: 2px solid white; */
+  .img {
+    width: 3%;
+    height: 3%;
+    position: absolute;
+    left: 26%;
+    top: 9%;
   }
 `;
+const MyRankingBody = styled.div`
+  width: 70%;
+  height: 70%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid yellow; */
+`;
+const MyRankingList = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid purple; */
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding-left: 1rem;
+  }
+`;
+const AllRankingBlock = styled.div`
+  width: 100%;
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid orange; */
+`;
+const AllRankingHeader = styled.div`
+  display: flex;
+  width: 90%;
+  height: 15%;
+  .img {
+    width: 3%;
+    height: 3%;
+    position: absolute;
+    left: 27%;
+    top: 29%;
+  }
+  /* border: 2px solid white; */
+`;
+const AllRankingList = styled.div`
+  width: 100%;
+  height: 85%;
 
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid purple; */
+`;
 function GameRankingPage() {
   const [myRank, setMyRank] = useState<{
     myAlgoRank: number;
@@ -53,8 +138,58 @@ function GameRankingPage() {
   };
   return (
     <Wrapper>
-      <h1>게임별 랭킹 페이지</h1>
-      <h2>나의 랭킹</h2>
+      <MainHeader>
+        <h1>🏆게츠비 명예의 전당🏆</h1>
+      </MainHeader>
+      <MyRankingBlock>
+        <MyRankingHeader>
+          <img className="img" src="/img/crown.png" alt="asdfasdfasdf" />
+          <h2>나의 랭킹</h2>
+        </MyRankingHeader>
+        <MyRankingBody>
+          <MyRankingList>
+            <h3>알고리즘</h3>
+            <div>
+              <h1>{myRank?.myAlgoRank}</h1>
+              <div>위</div>
+            </div>
+          </MyRankingList>
+          <MyRankingList>
+            <h3>CS 게임</h3>
+            <div>
+              <h1>{myRank?.myCsRank}</h1>
+              <div>위</div>
+            </div>
+          </MyRankingList>
+          <MyRankingList>
+            <h3>타자게임</h3>
+            <div>
+              <h1>{myRank?.myTypingRank}</h1>
+              <div>위</div>
+            </div>
+          </MyRankingList>
+          <MyRankingList>
+            <h3>싸피게임</h3>
+            <div>
+              <h1>{myRank?.myLuckRank}</h1>
+              <div>위</div>
+            </div>
+          </MyRankingList>
+        </MyRankingBody>
+      </MyRankingBlock>
+      <AllRankingBlock>
+        <AllRankingHeader>
+          <img className="img" src="/img/crown.png" alt="asdfasdfasdf" />
+          <h2>랭킹 TOP 5</h2>
+        </AllRankingHeader>
+        <AllRankingList>
+          <RankingInfo type={'알고리즘'} rankersInfo={algoRankers} />
+          <RankingInfo type={'CS 게임'} rankersInfo={CSRankers} />
+          <RankingInfo type={'타자게임'} rankersInfo={typingRankers} />
+          <RankingInfo type={'싸피게임'} rankersInfo={luckRankers} />
+        </AllRankingList>
+      </AllRankingBlock>
+      {/* <h1>게임별 랭킹 페이지</h1>
       <div className="myRankDiv">
         <div>
           <h3>알고리즘</h3>
@@ -88,7 +223,7 @@ function GameRankingPage() {
       )}
       {nowGame === 'casino' && (
         <RankingInfo type={nowGame} rankersInfo={luckRankers} />
-      )}
+      )} */}
     </Wrapper>
   );
 }
