c031c6d2a4631e813d027e82dc68ff988ffe5772
MinYongGwon <alsdyd320@naver.com>
2022-11-19 00:03:50 +0900

    [BE] Add: 친구 신청시 알람 오류 수정

--- backend/src/main/java/com/ssafy/gaese/domain/friends/application/FriendService.java
+++ backend/src/main/java/com/ssafy/gaese/domain/friends/application/FriendService.java
@@ -71,7 +71,7 @@ public class FriendService {
             friendRequestRepository.save(friendRequest);
         }
 
-        friendSocketService.friendAlarm(userId);
+        friendSocketService.friendAlarm(targetUser.getId());
         return true;
     }
 
