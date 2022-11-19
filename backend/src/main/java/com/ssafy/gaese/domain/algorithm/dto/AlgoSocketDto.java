644d16aaf2800e725e41216ca223e2d886771425
soojin <ksgf988@naver.com>
2022-11-20 00:17:32 +0900

    [BE] Add: 타이핑 방정원 설정 및 exceed 에러 핸들링

--- backend/src/main/java/com/ssafy/gaese/domain/algorithm/dto/AlgoSocketDto.java
+++ backend/src/main/java/com/ssafy/gaese/domain/algorithm/dto/AlgoSocketDto.java
@@ -6,7 +6,10 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Getter
+@Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @SuperBuilder
 public class AlgoSocketDto extends BaseSocketDto {
 
