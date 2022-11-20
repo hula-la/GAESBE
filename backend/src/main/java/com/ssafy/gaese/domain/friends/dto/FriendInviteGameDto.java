c6b3be906b5c66e8f2f6b04177d134a691d4a96f
soojin <ksgf988@naver.com>
2022-11-20 17:01:55 +0900

    [BE] Update: inviteDto id->nickname으로 받기

--- backend/src/main/java/com/ssafy/gaese/domain/friends/dto/FriendInviteGameDto.java
+++ backend/src/main/java/com/ssafy/gaese/domain/friends/dto/FriendInviteGameDto.java
@@ -11,7 +11,7 @@ import lombok.*;
 @Builder
 public class FriendInviteGameDto {
     private Long userId;
-    private Long fromUserId;
+    private String fromUserNick;
     private String gameType;
     private String roomCode;
 }
